4e4c2296efaf9e8250aebb7d017addaabbf17f29
Anthony Olivence <aolivence@gmail.com>
2013-10-11 02:29:16 -0400

    Added base project

---
+++ src/com/example/cheetahreader/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.cheetahreader;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://www.simplecta.com");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
